4767388f0485fc7993509d2d8de290edfd773318
varspijoyan <varsikpijoyan2003@gmail.com>
2025-03-04 18:18:39 +0400

    Added 'main' section

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Header from './components/header/Header';
-
+import MainPage from "./pages/MainPage"
 function App() {
   return (
     <>
       <Header />
+      <MainPage />
     </>
   );
 }
--- project/src/pages/MainPage.jsx
+++ project/src/pages/MainPage.jsx
@@ -1,6 +1,9 @@
+import MainSection from "../components/main-section/MainSection";
+
 export default function MainPage() {
     return (
         <>
+            <MainSection />
         </>
     );
 }
