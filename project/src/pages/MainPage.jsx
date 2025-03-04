df5df48315d117ea0dc327de76a14e9b8fcf910b
varspijoyan <varsikpijoyan2003@gmail.com>
2025-03-04 18:50:50 +0400

    Added 'clients' section

--- project/src/pages/MainPage.jsx
+++ project/src/pages/MainPage.jsx
@@ -1,9 +1,11 @@
+import ClientsSection from "../components/clients-section/ClientsSection";
 import MainSection from "../components/main-section/MainSection";
 
 export default function MainPage() {
     return (
         <>
             <MainSection />
+            <ClientsSection />
         </>
     );
 }
