c99b79a50b00f9429cb36d8be4b72b16b3ec2011
varspijoyan <varsikpijoyan2003@gmail.com>
2025-03-04 17:42:41 +0400

    Added media query for header and Hamburger component from hamburger-react

--- project/src/components/header/HeaderNavBar.jsx
+++ project/src/components/header/HeaderNavBar.jsx
@@ -1,15 +1,23 @@
+import { Sling as Hamburger } from "hamburger-react";
+import { useState } from "react";
 import styles from "../../styles/Header.module.css";
 
 export default function HeaderNavBar() {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <nav className={styles.navBar}>
-            <ul className={styles.ulLinks}>
-                <li className={styles.link}><a href="#">Home</a></li>
-                <li className={styles.link}><a href="#">Features</a></li>
-                <li className={styles.link}><a href="#">Community</a></li>
-                <li className={styles.link}><a href="#">Blog</a></li>
-                <li className={styles.link}><a href="#">Pricing</a></li>
-            </ul>
+            <div className={`${styles.navLinks} ${isOpen ? styles.show : ''}`}>
+                <ul className={styles.ulLinks}>
+                    <li className={styles.link}><a href="#">Home</a></li>
+                    <li className={styles.link}><a href="#">Features</a></li>
+                    <li className={styles.link}><a href="#">Community</a></li>
+                    <li className={styles.link}><a href="#">Blog</a></li>
+                    <li className={styles.link}><a href="#">Pricing</a></li>
+                </ul>
+            </div>
+            <div className={styles.hamburgerMenu}>
+                <Hamburger toggled={isOpen} toggle={setIsOpen}/>
+            </div>
             <button className={styles.btn}>Register Now</button>
         </nav>
     );
